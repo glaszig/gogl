b3ac4908612b734b9b2c353df6fdbffa755c9eb3
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-04-24 10:44:23 -0300

    fixed formatting to match the original

--- gogl_test.go
+++ gogl_test.go
@@ -3,20 +3,20 @@ package gogl
 import "testing"
 
 const (
-	ExampleUrl string = "http://www.example.com/"
-	ShortUrl   string = "http://goo.gl/U98s"
+  ExampleUrl string = "http://www.example.com/"
+  ShortUrl   string = "http://goo.gl/U98s"
 )
 
 func TestShorten(t *testing.T) {
-	result, _ := Shorten(ExampleUrl)
-	if result.Id != ShortUrl {
-		t.Errorf("Shorten().Kind is not 'urlshortener#url': %+v", result)
-	}
+  result, _ := Shorten(ExampleUrl)
+  if result.Id != ShortUrl {
+    t.Errorf("Shorten().Kind is not 'urlshortener#url': %+v", result)
+  }
 }
 
 func TestExpand(t *testing.T) {
-	result, _ := Expand(ShortUrl)
-	if result.LongUrl != ExampleUrl {
-		t.Errorf("Expand().LongUrl is not '%s': %+v", ExampleUrl, result)
-	}
+  result, _ := Expand(ShortUrl)
+  if result.LongUrl != ExampleUrl {
+    t.Errorf("Expand().LongUrl is not '%s': %+v", ExampleUrl, result)
+  }
 }
